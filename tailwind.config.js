f38da659f0134fd67fc8651b5a390b08d4789fdb
Victor Patru <patruvictor.mihai@gmail.com>
2022-08-26 16:34:45 +0300

    feat: create header and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        softRed: "hsl(7, 99%, 70%)",
+        yellow: "hsl(51, 100%, 49%)",
+        darkCyan: "hsl(167, 40%, 24%)",
+        darkBlue: "hsl(198, 62%, 26%)",
+        darkModerateCyan: "hsl(168, 34%, 41%)",
+        veryDarkBlue: "hsl(212, 27%, 19%)",
+        veryDarkGrayishBlue: "hsl(213, 9%, 39%)",
+        darkGrayishBlue: "hsl(232, 10%, 55%)",
+        grayishBlue: "hsl(210, 4%, 67%)",
+      },
+      fontFamily: {
+        sans: ["Barlow", "sans-serif"],
+        serif: ["Fraunces", "serif"],
+      },
+      backgroundImage: {
+        "header-mobile": "url('../images/mobile/image-header.jpg')",
+        "header-desktop": "url('../images/desktop/image-header.jpg')",
+        "transform-mobile": "url('../images/mobile/image-transform.jpg')",
+        "transform-desktop": "url('../images/mobile/image-transform.jpg')",
+      },
+    },
   },
   plugins: [],
 };
